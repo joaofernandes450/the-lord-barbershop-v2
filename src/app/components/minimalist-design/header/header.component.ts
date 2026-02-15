c35d071ca445bdddfc390e890f84a6381f1594d3
joaofernandes450 <78604293+joaofernandes450@users.noreply.github.com>
2026-02-15 19:50:26 +0000

    Header mobile overlay & improvements

--- src/app/components/minimalist-design/header/header.component.ts
+++ src/app/components/minimalist-design/header/header.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [AsyncPipe, MatIconModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private readonly breakpointObserver = inject(BreakpointObserver);
 
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay(),
+    );
+
+  showMobileContent = false;
 }
