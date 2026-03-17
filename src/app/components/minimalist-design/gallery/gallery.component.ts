c6fa8af0e2d1988e26445f2c7c1b918f58c220c6
joaofernandes450 <78604293+joaofernandes450@users.noreply.github.com>
2026-03-17 21:41:09 +0000

    Change social & gallery images

--- src/app/components/minimalist-design/gallery/gallery.component.ts
+++ src/app/components/minimalist-design/gallery/gallery.component.ts
@@ -8,20 +8,11 @@ import { Component } from '@angular/core';
 })
 export class GalleryComponent {
   images = [
-    'assets/slider/1.png',
-    'assets/slider/2.png',
-    'assets/slider/3.png',
-    'assets/slider/4.png',
-    'assets/slider/5.png',
-    'assets/slider/6.png',
-    // 'assets/slider/7.png',
-    // 'assets/slider/8.png',
-    // 'assets/slider/9.png',
-    // 'assets/slider/10.png',
-    // 'assets/slider/11.png',
-    // 'assets/slider/12.png',
-    // 'assets/slider/13.png',
-    // 'assets/slider/14.png',
-    // 'assets/slider/15.png',
+    'assets/gallery/1.png',
+    'assets/gallery/2.png',
+    'assets/gallery/3.png',
+    'assets/gallery/4.png',
+    'assets/gallery/5.png',
+    'assets/gallery/6.png',
   ];
 }
